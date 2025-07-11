eacdd1aa6604934c0f932a9604f25b27d2777009
robinjusticeleague <robinjusticeleague@gmail.com>
2025-07-11 12:56:23 +0000

    feat: monetag

--- www/app/page.tsx
+++ www/app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://otieu.com/4/9558265"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Support us by seeing ads →
         </a>
       </footer>
     </div>
