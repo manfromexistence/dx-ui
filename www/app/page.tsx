7dcff9a801e21d2613d16c0a02ef83b5fe13a690
robinjusticeleague <robinjusticeleague@gmail.com>
2025-07-17 01:57:40 +0000

    feat: sound types

--- www/app/page.tsx
+++ www/app/page.tsx
@@ -1,9 +1,17 @@
 "use client"
 
+import useSound from "@/lib/sound";
+
+const BoopButton = () => {
+    const [play] = useSound("/click.mp3");
+    return <div onClick={play}>Boop!</div>;
+};
+
 export default function HomePage() {
     return (
         <div className="h-full w-full flex items-center justify-center font-bold text-4xl">
             <span>DX SOUND</span>
+            <BoopButton />
         </div>
     );
 }
